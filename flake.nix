cf19b89514a4efbd2097e8305a489fafa0ae026b
Roman Krasilnikov <x0kill@ya.ru>
2026-06-02 19:39:39 +0300

    chore(sv-utils): bump yaml dependency to 3.0.0-1

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs_24
+            pkgs.pnpm
+          ];
+        };
+      };
+    };
+}
